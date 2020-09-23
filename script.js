322fc34c5d50dea7423824d1df952c92a2d96966
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-09-22 19:11:36 -0700

    Current Weather section works and populates correct info

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+// When the 'Search Button' is clicked
 $("#search").on("click", function () {
 
+    // My API Key for the weather.api
     let api_key = "54e8632607a7b7305b56a20d56ee0b2e";
-    let cityName = "Phoenix";
+    
+    // Get the user entered City from the HTML input field
+    let cityEntry = ($("#cityEntry").val());
 
-    let currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + api_key;
-
-    let forecastWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + api_key;
+    // Reach out to the API(s) to get the Weather & Forecast
+    let currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityEntry + "&appid=" + api_key;
+    let forecastWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityEntry + "&appid=" + api_key;
 
 
     $.ajax({
@@ -13,8 +17,25 @@ $("#search").on("click", function () {
         method: "GET"
     }).
         then(function (response) {
-            console.log("Current Weather")
-            console.log(response);
+                       
+            let cityName = $("<h2>");
+            cityName.text(response.name);
+            $("#currentWeather").append(cityName);
+            
+            let temperature = $("<h5>");
+            let kelvinTemp = response.main.temp;
+            let fahrenheitTemp = parseInt((kelvinTemp - 273.15) * (9/5) + 32);
+            temperature.text("Temperature: " + fahrenheitTemp + " F");
+            $("#currentWeather").append(temperature);
+
+            let humidity = $("<h5>");
+            humidity.text("Humidity: " + response.main.humidity + " %");
+            $("#currentWeather").append(humidity);
+
+            let windSpeed = $("<h5>");
+            windSpeed.text("Wind Speed: " + response.wind.speed + " MPH");
+            $("#currentWeather").append(windSpeed);
+            
 
             $.ajax({
                 url: forecastWeatherURL,
