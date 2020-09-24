ae0840f82be8c6da52c309dc4088e2810823ccd2
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-09-23 23:05:25 -0700

    City history section now adds buttons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // When the 'Search Button' is clicked
-$("#search").on("click", function () {
+$(".btn").on("click", function () {
 
     // My API Key for the weather.api
     let api_key = "54e8632607a7b7305b56a20d56ee0b2e";
@@ -7,37 +7,43 @@ $("#search").on("click", function () {
     // Get the user entered City from the HTML input field
     let cityEntry = ($("#cityEntry").val());
 
+    // Empty array into which the user selected cities are 'pushed into'
+    let cityHistory = [];
+    cityHistory.push(cityEntry);
+
+    // Loop through the array and create a button for each city name
+    for (let i = 0; i < cityHistory.length; i++) {
+        let cityButton = $("<button>");
+        cityButton.html('<button class="btn btn-primary" type="button">' + cityHistory[i] + '</button');
+        $("#searchHistory").append(cityButton);
+    };
+
+
     // Reach out to the API(s) to get the Weather & Forecast
     let currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityEntry + "&appid=" + api_key;
     let forecastWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityEntry + "&appid=" + api_key;
 
 
-
+    // Call to reach out to the API and get current weather info
     $.ajax({
         url: currentWeatherURL,
         method: "GET"
     }).
         then(function (response) {
-            console.log("Current Weather:")
-            console.log(response);
-
-            // Today's Date
+            
+            // Today's Date printed to HTML
             let currentTime = moment().format("MMM Do YYYY");
             let today = $("<h1>");
             today.text(currentTime);
             $("#currentWeather").append(today);
 
-            // Print the user entered City Name
+            // Print the user entered City Name from input field & Icon from API data for the weather
             let cityName = $("<h2>");
             let icon = $("<img>");
             icon.attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png")
             cityName.text(response.name);
             $("#currentWeather").append(cityName);
             $("#currentWeather").append(icon);
-
-
-
-
 
             // Print the current temperature of the user entered City **ALSO** converts from Kelvin to F
             let temperature = $("<h5>");
@@ -56,23 +62,25 @@ $("#search").on("click", function () {
             windSpeed.text("Wind Speed: " + response.wind.speed + " MPH");
             $("#currentWeather").append(windSpeed);
 
-            // Pull the Lattitude and Longitude from current weather API
+
+            // Pull the Lattitude and Longitude for the user entered city from current weather API
             let lat = response.coord.lat;
             let lon = response.coord.lon;
 
+            // Take Lattitude and Longitude to the uvIndex API to get infromation
             let uvIndexURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + api_key;
 
+            // Call to the uvIndex API
             $.ajax({
                 url: uvIndexURL,
                 method: "GET"
             }).
                 then(function (response) {
-                    console.log("UV Index: ");
-                    console.log(response);
-
+                    
+                    // Pull the uvIndex value from the API data
                     let uvIndex = response.value;
-                    console.log(uvIndex);
-
+                    
+                    // Evaluates the value of the uvIndex and color codes it accordingly from the style.css file
                     if (uvIndex >= 7) {
                         displayIndex = $("<h5>");
                         displayIndex.html("UV Index: " + '<span class="severe">' + uvIndex + '</span>');
@@ -86,48 +94,53 @@ $("#search").on("click", function () {
                         displayIndex.html("UV Index: " + '<span class="favorable">' + uvIndex + '</span>');
                         $("#currentWeather").append(displayIndex);
                     }
-
                 });
 
-
+            // Call to the forecast API to get 5-day forecast weather
             $.ajax({
                 url: forecastWeatherURL,
                 method: "GET"
             }).
                 then(function (response) {
-                    console.log("Forecast Weather: ");
-                    console.log(response);
+                    // Empties the 'html container' to ensure things don't "stack up"
+                    $("#fiveDay").empty();
 
-                    let newDiv = $("<div>");
-                    newDiv.addClass("card text-white bg-primary mb-");
-                    newDiv.attr("width", "18rem")
+                    for (let i = 0; i < 5; i++) {
+                        // Creates a new Div to store the 5-day forecast
+                        let newDiv = $("<div>");
+                        newDiv.addClass("row");
+                        //newDiv.addClass("card text-white bg-primary mb-");
+                        // newDiv.attr("width", "18rem")
 
-                    let futureDate = $("<h1>");
-                    futureDate = moment().add(1, 'day').format('MM/DD/YYYY');
-                    newDiv.append(futureDate);
+                        // Future date
+                        let futureDate = $("<h1>");
+                        futureDate = moment().add((i + 1), 'day').format('MM/DD/YYYY');
+                        newDiv.append(futureDate);
 
-                    let newIcon = $("<img>");              
-                    newIcon.attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png");
-                    newDiv.append(newIcon);
+                        // Weather icon for the future date
+                        let newIcon = $("<img>");
+                        newIcon.attr("src", "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png");
+                        newDiv.append(newIcon);
 
+                        // Future date temp converted to F
+                        let kelvinTemp = response.list[i].main.temp;
+                        let fahrenheitTemp = parseInt(
+                            (kelvinTemp - 273.15) * (9 / 5) + 32);
 
+                        // Displays the future temp
+                        let displayTemp = $("<p>");
+                        displayTemp.text("Temp: " + fahrenheitTemp + " F ");
+                        newDiv.append(displayTemp);
 
-                    let kelvinTemp = response.list[0].main.temp;
-                    let fahrenheitTemp = parseInt(
-                    (kelvinTemp - 273.15) * (9 / 5) + 32);
-                    
-                    let displayTemp = $("<p>");
-                    displayTemp.text("Temp: " + fahrenheitTemp + " F");
-                    newDiv.append(displayTemp);
+                        // Displays the future humidity
+                        let displayHumidity = $("<p>");
+                        displayHumidity.text("Humidity: " + response.list[0].main.humidity + " %");
+                        newDiv.append(displayHumidity);
 
-                    let displayHumidity = $("<p>");
-                    displayHumidity.text("Humidity: " + response.list[0].main.humidity + " %");
-                    newDiv.append(displayHumidity);
+                        // Attaches the newly created div to the HTML
+                        $("#fiveDay").append(newDiv);
 
-
-                    $("#fiveDay").append(newDiv);
-
-
+                    };
 
 
 
@@ -139,7 +152,6 @@ $("#search").on("click", function () {
         });
 
 });
-
 
 
 
